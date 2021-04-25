32e42d8041b83fdf7667b5ef038abba25feefc64
freyhoe <61102138+freyhoe@users.noreply.github.com>
2021-04-25 10:35:32 -0500

    BB
    BB

--- Levels/popup.gd
+++ Levels/popup.gd
@@ -8,9 +8,9 @@ onready var SPop:Popup = find_node("sliderPrompt")
 var timer = 0
 var budget = 10000000
 var population = 10000
-var happiness = 10000
+var happiness = 50000
 var weeks = 0
-var environment = 100000
+var environment = 50000
 
 var money_drain = 0
 
@@ -55,22 +55,109 @@ func _on_Prompt_answer(choice):
 			Pop.start("What do you choose?","stay plastic (-$100 weekly)","go paper (-$500 weekly)", "go metal (+$8000 one time)")
 		10:
 			Game.fade()
-			next_week()
 			if(choice == 1):
 				happiness+=3
 				environment-=20000
-				money_drain-=100
+				money_drain+=100
 				Pop.start("2 ducks were found dead with plastic straws in their throats")
 			elif (choice == 2):
-				money_drain-=500
+				money_drain+=500
 				Pop.start("Paper Landfill buildup crushes a man")
 				population -=1
 			else:
 				happiness-=5
 				environment +=30000
 				budget+=8000
-				Pop.start("local moms concerned that metal straws will be used as a drug pipe")
-				
+				Pop.start("local moms concerned that metal straws will be used as a drug pipes")
+			next_week()
+		11:
+			Pop.start("Currently, your city follows the same convention as your country of Themerica when it comes to grocery stores - plastic is widespread and conventionally used in packaging")
+		12:
+			Pop.start("However, like stores such as Baldis, GostGo, BullsEye, and Farmer Joes, you can try and remove as much plastic from your towns grocery stores as possible.")
+		13:
+			SPop.start("There are 17 stores, for each store reducing plastic: -$50000","reduced plastic stores","","",17)
+		15:
+			Pop.start("You need to reach 50,000 kWh units of energy per day.")
+		16:
+			SPop.start("Partition the amount of each energy (10000kwh each) you will use.", "nuclear -3mil", "fossil fuel -2mil", "hydro -1mil", 5)
+		18:
+			Pop.start("Currently, Oovoo Java has an illegal dumping problem with corporations who want to avoid the legal process of disposing of hazardous materials.")
+		19:
+			Pop.start("This hazardous waste could taint water and destroy ecosystems. However, the company has the resources to just continue to dump waste in this manner and pay fines.")
+		20:
+			Pop.start("Sadly, this hasn’t gotten much media coverage. What do you do?", "Enhance regulations","Accept fines +$500000")
+		21:
+			Game.fade()
+			next_week()
+			if(choice == 2):
+				budget+=500000
+				environment-=4000
+				Pop.start("the media picks up on what you did, and the action is broadcasted across the world")
+			else:
+				environment+=8000
+				Pop.start("companies see you cracking down on regulations, and lower their pollutants")
+		22:
+			Pop.start("As of right now, everyone in oovoo java likes to travel with a car - it’s fast and convenient. However, this produces a lot of emissions in the environment.")
+		23:
+			Pop.start("However, you have the option to build more environmentally conscious infrastructure.", "Build a railroad -$100000","Do nothing")
+		24:
+			Game.fade()
+			next_week()
+			if(choice == 1):
+				environment+=4000
+				budget-=100000
+				get_parent().get_node("togglable3").visible = false
+				yield(get_tree().create_timer(3), "timeout")
+			else:
+				environment-=6000
+			Pop.start("Now that the railroad is built, bike riding is safer and more people bike")
+		25:
+			Pop.start("Currently, the people who do ride cars still emit many unnecessary greenhouse gasses")
+		26:
+			Pop.start("Would you like to regulate the quality of oil, raising the price but greatly lowering emissions?","Regulate gas -$100000","Dont lower")
+		27:
+			Game.fade()
+			next_week()
+			if(choice == 1):
+				happiness-=3000
+				budget-=100000
+				environment+=10000
+			else:
+				happiness+=2000
+				environment-=1000
+			Pop.start("Sadly, the citizens of Oovoo Java have held an insurrection and booted you out! They are very displeased with your governing")
+		28:
+			Pop.start("Remember, the citizens resist change and don’t know very much about climate change! Try and reach a balance next time.")
+		29:
+			Pop.start("Oh no! Your poor management of the environment has doomed Oovoo Java. The sky is riddled with pollution, and living here is unhealthy.")
+		30:
+			Pop.start("Your town has contributed to drastic climate change, and ecosystems are disrupted. Many organisms are dying.")
+		31:
+			Pop.start("This is a very real problem that the earth is facing - with the current rates of air pollution and the release of greenhouse gasses")
+		32:
+			Pop.start("The earth’s climate is projected to change more dramatically in the next 50 years than it has in the last 6000.")
+		33:
+			Pop.start("In order to combat this, we must take many of the measures offered in this video game - to lower emissions, ")
+		34:
+			Pop.start("we must find cleaner methods of transportation, shipping, manufacturing, and energy production.")
+		35:
+			Pop.start("As it is currently, our lifestyle is not sustainable. We need to change.")
+		36:
+			Pop.start("Corporations must be held accountable, lobbying must end, and those who are able to make a change, the government, must listen to our voices. ")
+		37:
+			Pop.start("You can write to your mayor, governor, or even congress about climate change.")
+		38:
+			Pop.start("Our personal lifestyles must be accounted for as well. Do your best not to pollute the environment with litter, ")
+		39:
+			Pop.start("Try and find better sources of energy, bike more, use public transportation - there are so many methods to reduce our negative impact on the environment!")
+		40:
+			Pop.start("Plus, we have gotten to a point where we as humans NEED to change, and do so quickly.")
+		41:
+			Pop.start("After all, it’d be great to see a future on this planet.")
+		42:
+			Pop.start("Thanks for playing our game!")
+		43:
+			Game.emit_signal("ChangeScene", 'res://MainMenu/MainMenu.tscn')
 	update_hud()
 
 
@@ -94,6 +181,26 @@ func _on_sliderPrompt_answer(a1, a2, a3):
 				happiness+=1
 				environment-=2
 				s = "chicken ninjas sneak into the granary, caught after stuffing themselves"
-			Pop.start("You built a cattle ranch, a sythetic meat lab, and a granary. "+s)
 			get_parent().get_node("togglable").visible = true
+			yield(get_tree().create_timer(3), "timeout")
+			Pop.start("You built a cattle ranch, a sythetic meat lab, and a granary. "+s)
+		14:
+			budget -= a1*50000
+			Game.fade()
+			next_week()
+			if(a1>=20):
+				Pop.start("many stores reduced their plastic usage, ducks and other aquatic life parade in the streets")
+			elif(a1==0):
+				Pop.start("no stores reduced plastic, families of ducks show up in protest")
+			else:
+				Pop.start("some stores reduced their plastic usage")
+		17:
+			budget -= 3000000*a1 + 2000000*a2 + 1000000*a3
+			happiness -=6000*a1+2000*a2-1000*a3
+			environment += 10000*a1 -35000*a2-40000*a3
+			Game.fade()
+			next_week()
+			get_parent().get_node("togglable2").visible = true
+			yield(get_tree().create_timer(3), "timeout")
+			Pop.start("Deforestation and the damming of lakes for hydro power causes intesnse ecological trauma")
 	update_hud()
--- addons/GameTemplate/GUI/Prompt/sliderPrompt.gd
+++ addons/GameTemplate/GUI/Prompt/sliderPrompt.gd
@@ -20,12 +20,13 @@ func _ready()->void:
 	retranslate()
 
 
-func start(p, a1="next", a2="", a3="",m=1000):
+func start(p, a1="next", a2="", a3="",ezfrag=0):
 	prompt = p
 	ans1 = tr(a1)
 	ans2 = tr(a2)
 	ans3 = tr(a3)
-	budget = m
+	budget = ezfrag
+	
 	update_max()
 	popup()
 
@@ -62,9 +63,17 @@ func _on_ans3_value_changed(value):
 	find_node("ans2").get_node("HSlider").max_value = budget-a1-a3
 
 func update_max():
+	find_node("ans1").get_node("HSlider").value = 0
+	find_node("ans2").get_node("HSlider").value = 0
+	find_node("ans3").get_node("HSlider").value = 0
+
 	find_node("ans1").get_node("HSlider").max_value = budget
 	find_node("ans2").get_node("HSlider").max_value = budget
 	find_node("ans3").get_node("HSlider").max_value = budget
+	find_node("ans1").get_node("HSlider").min_value = 0
+	find_node("ans2").get_node("HSlider").min_value = 0
+	find_node("ans3").get_node("HSlider").min_value = 0
+
 
 func _on_DefaultButton_pressed():
 	if a1+a2+a3>budget:
